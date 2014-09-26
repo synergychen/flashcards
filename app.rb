0e73b3ba90272b4d3708434c913d0262c1aa03c4
ChenHuang <chenhuang@ChenHuangs-MacBook-Pro.local>
2014-09-26 16:05:16 -0400

    Update deck

--- app.rb
+++ app.rb
@@ -29,10 +29,27 @@ post "/decks" do
   new_deck_name = params[:deck][:name]
   Deck.create(name: new_deck_name)
 
-  redirect to("/")
+  redirect to "/"
 end
 
+# show
 get "/decks/:id" do
   @deck = Deck.find(params[:id])
   erb :deck
+end
+
+# edit
+get "/decks/:id/edit" do
+  @deck = Deck.find(params[:id])
+  erb :edit_deck
+end
+
+# update
+patch "/decks/:id" do
+  @deck = Deck.find(params[:id])
+  p @deck.inspect
+  update_deck_name = params[:deck][:name]
+  @deck.update(name: update_deck_name)
+
+  redirect to "/"
 end
