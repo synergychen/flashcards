fe20ad7f6e7941eb5dcebd8cbcf275ec93b90001
ChenHuang <chenhuang@ChenHuangs-MacBook-Pro.local>
2014-09-26 15:39:23 -0400

    Create a new deck

--- app.rb
+++ app.rb
@@ -19,6 +19,19 @@ get "/" do
   erb :home
 end
 
+# new
+get "/decks/new" do
+  erb :new_deck
+end
+
+# create
+post "/decks" do
+  new_deck_name = params[:deck][:name]
+  Deck.create(name: new_deck_name)
+
+  redirect to("/")
+end
+
 get "/decks/:id" do
   @deck = Deck.find(params[:id])
   erb :deck
