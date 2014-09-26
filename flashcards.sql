3969734d7057c5c82297632be9caebf607676222
ChenHuang <chenhuang@ChenHuangs-MacBook-Pro.local>
2014-09-26 15:38:50 -0400

    Set id as serial and delete id in INSERT argument

--- flashcards.sql
+++ flashcards.sql
@@ -2,30 +2,27 @@ DROP TABLE decks;
 DROP TABLE cards;
 
 CREATE TABLE decks(
-  id integer NOT NULL,
+  id serial PRIMARY KEY,
   name character varying(255)
 );
 
 CREATE TABLE cards(
-  id integer NOT NULL,
+  id serial PRIMARY KEY,
   front character varying(255),
   back character varying(255),
   deck_id integer NOT NULL
 );
 
-INSERT INTO decks (id, name)
+INSERT INTO decks (name)
   VALUES
-    (1, 'japanese'),
-    (2, 'germany');
+    ('japanese'),
+    ('germany');
 
-INSERT INTO cards (id, front, back, deck_id)
+INSERT INTO cards (front, back, deck_id)
   VALUES
-    (1, 'Dog', 'Inu', 1),
-    (2, 'Cat', 'Neko', 1),
-    (3, 'Snake', 'Hebi', 1),
-    (4, 'Dog', 'Hund', 2),
-    (5, 'Cat',  'Katze', 2),
-    (6, 'Snake', 'Schlange', 2);
-
-ALTER TABLE ONLY decks
-    ADD CONSTRAINT decks_pkey PRIMARY KEY (id);
+    ('Dog', 'Inu', 1),
+    ('Cat', 'Neko', 1),
+    ('Snake', 'Hebi', 1),
+    ('Dog', 'Hund', 2),
+    ('Cat',  'Katze', 2),
+    ('Snake', 'Schlange', 2);
